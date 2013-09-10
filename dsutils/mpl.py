53816a589902165c056c6c78652d0b9f64eb387a
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2013-09-10 10:17:32 +0300

    added mpl brewer config and remove_border from Chris B.

---
+++ dsutils/mpl.py
@@ -0,0 +1,53 @@
+"""Utilities for matplotlib.
+
+Code is adopted from Chris Beaumont, https://github.com/cs109/content
+
+It uses brewer2mpl for brewer color paletts.
+"""
+import brewer2mpl
+
+from matplotlib import pyplot as plt
+
+# Set up some better defaults for matplotlib
+from matplotlib import rcParams
+
+#colorbrewer2 Dark2 qualitative color table
+dark2_colors = brewer2mpl.get_map('Dark2', 'Qualitative', 7).mpl_colors
+
+rcParams['figure.figsize'] = (10, 6)
+rcParams['figure.dpi'] = 150
+rcParams['axes.color_cycle'] = dark2_colors
+rcParams['lines.linewidth'] = 2
+rcParams['axes.facecolor'] = 'white'
+rcParams['font.size'] = 14
+rcParams['patch.edgecolor'] = 'white'
+rcParams['patch.facecolor'] = dark2_colors[0]
+rcParams['font.family'] = 'StixGeneral'
+
+
+def remove_border(axes=None, top=False, right=False, left=True, bottom=True):
+    """
+    Minimize chartjunk by stripping out unnecesasry plot borders and axis ticks
+
+    The top/right/left/bottom keywords toggle whether the corresponding plot
+    border is drawn
+    """
+    ax = axes or plt.gca()
+    ax.spines['top'].set_visible(top)
+    ax.spines['right'].set_visible(right)
+    ax.spines['left'].set_visible(left)
+    ax.spines['bottom'].set_visible(bottom)
+
+    #turn off all ticks
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
+
+    #now re-enable visibles
+    if top:
+        ax.xaxis.tick_top()
+    if bottom:
+        ax.xaxis.tick_bottom()
+    if left:
+        ax.yaxis.tick_left()
+    if right:
+        ax.yaxis.tick_right()
